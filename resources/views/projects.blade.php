b6e5c949f3dc6b6830fcfa99eca35c8d1dedfbb1
cacutler <116305038+cacutler@users.noreply.github.com>
2025-09-29 18:33:39 -0600

    Updated projects page

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -2,4 +2,39 @@
 @section('title', 'Projects')
 @section('content')
 <h1>Projects</h1>
+<div class="grid">
+    <div class="project-card">
+        <h2>CutlerTech Website</h2>
+        <a href="#"><img src="{{asset('images/project_image.png')}}" alt="CutlerTech Website link"></a>
+        <b>Web Development</b>
+        <p>Stack: Laravel</p>
+        <p>Developer: Alex Cutler</p>
+    </div>
+</div>
+<style>
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(1, 1fr);
+        grid-template-rows: auto;
+    }
+    .project-card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        border: 3px solid black;
+        width: 25%;
+        margin: 1rem auto;
+        text-align: center;
+        padding: 0.5rem;
+        background-color: #ffa900;
+    }
+    .project-card img {
+        width: 100%;
+        height: auto;
+        padding: 1rem;
+    }
+    p {
+        padding: 0 !important;
+    }
+</style>
 @endsection
