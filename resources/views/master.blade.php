17c1af0af121438552cfd0af88187fc572826273
cacutler <116305038+cacutler@users.noreply.github.com>
2025-11-11 12:19:10 -0700

    Added a favicon

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('css/main.css')}}">
+        <link rel="icon" href="{{asset('images/logo.svg')}}" type="image/svg+xml">
     </head>
     <body>
         <header>
